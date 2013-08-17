20424ddc626588aea9e817da57035f6ed9e78103
SpektralDevelopment <spektraldevelopment@gmail.com>
2013-08-17 15:34:39 -0400

    Changed Spektral logo to <a> tag that links to spektraldevelopment.com/projectsite/ List item styling. Added copyright. Add linked in.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+(function(){
 
+    //Initialize Functions
+    initCopyright();
+
+    function initCopyright()
+    {
+       var d = new Date();
+       var y = d.getFullYear();
+
+       var currentYear = document.getElementById("copyright");
+       var copyString = "Copyright &copy; " + y;
+       currentYear.innerHTML = copyString;
+    }
+
+    console.log('Init Spektral Projects');
+})();

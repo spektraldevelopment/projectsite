23d99447cd619ab187acecd91b403da11a5f455a
SpektralDevelopment <spektraldevelopment@gmail.com>
2013-08-19 11:39:56 -0400

    Fixed scroll animation and added easing.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-(function(){
+$(document).ready(function(){
 
     //Vars
+    var logo = document.getElementById('logo');
 
     //AddEventListeners
+    listenEvent(logo, 'click', onLogoClick);
 
     //Initialize Functions
     initCopyright();
 
+    //////////////////
+    ////INIT COPYRIGHT
+    /////////////////
     function initCopyright()
     {
        var d = new Date();
@@ -17,20 +22,54 @@
        currentYear.innerHTML = copyString;
     }
 
-    $(document).ready(function() {
-        $('logo').click(function(){
-            $('html, body').animate({scrollTop : 0},800, done());
-            return false;
-        });
-        console.log( "ready!" );
-    });
-
-    function done()
+    //////////////////
+    ////ON LOGO CLICK
+    /////////////////
+    function onLogoClick(e)
     {
-        console.log("Animation Complete");
+        $('html:not(:animated), body:not(:animated)').animate( { scrollTop : "0px" }, 800, "easeOutExpo");
+        e.preventDefault();
+        return false;
+        console.log('logo Clicked!: ');
     }
 
-    console.log('Init Spektral Projects');
-})();
+    //////////////////
+    ////SCROLL TO ELEMENT
+    /////////////////
+    function scrollToElement( target )
+    {
+        var topoffset = 30;
+        var speed = 800;
+        var destination = jQuery( target ).offset().top - topoffset;
+        jQuery( 'html:not(:animated), body:not(:animated)' ).animate( { scrollTop: destination}, speed, function() {
+            window.location.hash = target;
+        });
+        return false;
+    }
+
+    //////////////////
+    ////LISTEN EVENT
+    /////////////////
+        function listenEvent(eventTarget, eventType, eventHandler)
+        {
+            if(eventTarget.addEventListener)
+            {
+                eventTarget.addEventListener(eventType, eventHandler, false)
+            }
+            else if(eventTarget.attachEvent)
+            {
+                eventType = "on" + eventType;
+                eventTarget.attachEvent(eventType, eventHandler)
+            }
+            else
+            {
+                eventTarget["on" + eventType] = eventHandler;
+            }
+        }
+
+        console.log('Init Spektral Projects');
+});
+
+
 
 
